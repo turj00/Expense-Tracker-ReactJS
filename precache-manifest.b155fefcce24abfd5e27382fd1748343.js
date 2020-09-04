f949521bf7f168a27329090c1a242d8591c71e2b
turj00 <turjo1331@gmail.com>
2020-09-05 02:28:34 +0600

    Updates

---
+++ precache-manifest.b155fefcce24abfd5e27382fd1748343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2e4cb4d1db6b2891ccbab579efebb4",
+    "url": "/Expense-Tracker-ReactJS/index.html"
+  },
+  {
+    "revision": "3a118cbe01a5e45c6ec6",
+    "url": "/Expense-Tracker-ReactJS/static/css/main.8e9560df.chunk.css"
+  },
+  {
+    "revision": "8933d4a772c3b8238a6a",
+    "url": "/Expense-Tracker-ReactJS/static/js/2.b33b0dce.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Expense-Tracker-ReactJS/static/js/2.b33b0dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a118cbe01a5e45c6ec6",
+    "url": "/Expense-Tracker-ReactJS/static/js/main.6adf02a2.chunk.js"
+  },
+  {
+    "revision": "a90a39f9ded9c2a270b3",
+    "url": "/Expense-Tracker-ReactJS/static/js/runtime-main.de0092a3.js"
+  }
+]);
